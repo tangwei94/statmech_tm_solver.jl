95e22a5be3ba49c3aa7d9a6a1b290d9e09bda225
Wei Tang <tangwei@smail.nju.edu.cn>
2021-11-05 17:20:58 +0100

    file from prev repo only for auxiliary use will rm

---
+++ test_with_TensorMap.jl
@@ -0,0 +1,297 @@
+function ovlp(psi::TensorMap{CartesianSpace, 2, 1}, phi::TensorMap{CartesianSpace, 2, 1})
+    chi_psi, chi_phi = dim(domain(psi)), dim(domain(phi))
+    v0 = Tensor(rand, Float64, ℝ^chi_psi*ℝ^chi_phi)
+    w, _ = eigsolve(transf_mat(psi, phi), v0, 1)
+    return w[1]
+end
+
+function rrule(::typeof(ovlp), psi::TensorMap{CartesianSpace, 2, 1}, phi::TensorMap{CartesianSpace, 2, 1})
+    chi_psi, chi_phi = dim(domain(psi)), dim(domain(phi))
+    v0 = Tensor(rand, Float64, ℝ^chi_psi*ℝ^chi_phi)
+    wr, vr = eigsolve(transf_mat(psi, phi), v0, 1)
+    _, vl = eigsolve(transf_mat_T(psi, phi), v0, 1)
+    fwd = wr[1]
+    vl, vr = vl[1], vr[1]
+
+    function ovlp_pushback(f̄wd)
+        prefactor = f̄wd / (vl' * vr)[1]
+        psi_pushback = @ncon((vl, phi, vr), ([-2, 1], [1, -3, 2], [-1, 2]))
+        psi_pushback = prefactor*adjoint(permute(psi_pushback, (1,), (2, 3)))
+        phi_pushback = @ncon((vl, psi', vr), ([1, -1], [2, 1, -2], [2, -3]))
+        phi_pushback = prefactor*permute(phi_pushback, (1, 2), (3,))
+        return NoTangent(), psi_pushback, phi_pushback
+    end
+    return fwd, ovlp_pushback
+end
+
+psi = TensorMap(rand, Float64, ℝ^5*ℝ^2, ℝ^5)
+Tpsi = act(T, psi)
+lop = transf_mat(psi, Tpsi)
+wr, vr = eigsolve(lop, Tensor(rand, Float64, ℝ^5*ℝ^10), 1)
+wr[1]
+lop_T = transf_mat_T(psi, Tpsi)
+wl, vl = eigsolve(lop_T, Tensor(rand, Float64, ℝ^5*ℝ^10), 1)
+wl[1]
+vl, vr = vl[1], vr[1]
+
+vl[1]' * vr[1]
+ovlp(psi, Tpsi)
+
+psi = TensorMap(rand, Float64, ℝ^5*ℝ^2, ℝ^5)
+lop = transf_mat(psi, psi)
+wr, vr = eigsolve(lop, Tensor(rand, Float64, ℝ^5*ℝ^5), 1)
+wr, vr = wr[1], vr[1]
+vr = permute(vr, (1, ), (2, ))
+eigh(vr)
+
+
+#grad(central_fdm(5,1), psi->real(ovlp(psi, psi)), psi)[1]
+# gradient check
+for ix in 1:10
+    Δpsi = 1e-6*TensorMap(rand, Float64, ℝ^5*ℝ^2, ℝ^5)
+    Δy = (real(ovlp(psi+Δpsi, psi+Δpsi)) - real(ovlp(psi-Δpsi, psi-Δpsi))) / 2
+    dpsi = gradient(psi->real(ovlp(psi, psi)), psi)[1]
+    Δy1 = dot(Δpsi, dpsi)
+    print(abs(Δy - Δy1) / abs(Δy) < 1e-8, ' ')
+end
+
+function check_act(phi::TensorMap{CartesianSpace, 2, 1})
+    Tphi = act(T, phi)
+    return real(ovlp(phi, Tphi))
+end
+
+for ix in 1:10
+    Δpsi = 1e-6*TensorMap(rand, Float64, ℝ^5*ℝ^2, ℝ^5)
+    Δy = (check_act(psi+Δpsi) - check_act(psi-Δpsi)) / 2
+    dpsi = gradient(check_act, psi)[1]
+    Δy1 = dot(Δpsi, dpsi)
+    print(abs(Δy - Δy1) / abs(Δy) < 1e-8, ' ')
+end
+
+function data_to_TensorMap(psidata::Array{Float64, 3})
+    chi, _, _ = size(psidata)
+    return TensorMap(psidata, ℝ^chi*ℝ^2, ℝ^chi)
+end
+function rrule(::typeof(data_to_TensorMap), psidata::Array{Float64, 3})
+    chi, _, _ = size(psidata)
+    fwd = TensorMap(psidata, ℝ^chi*ℝ^2, ℝ^chi)
+    function data_to_TensorMap_pushback(f̄wd)
+        return NoTangent(), reshape(f̄wd.data, (chi, 2, chi))
+    end
+    return fwd, data_to_TensorMap_pushback
+end
+
+# simple test
+psidata = rand(Float64, (4, 2, 4))
+psi = data_to_TensorMap(psidata)
+reshape(psi.data, (4,2,4))
+function check_data_to_TensorMap(psidata::Array{Float64, 3})
+    psi = data_to_TensorMap(psidata)
+    return real(ovlp(psi, psi))
+end
+check_data_to_TensorMap(psidata)
+gradient(check_data_to_TensorMap, psidata)
+gradient(x->real(ovlp(x, x)), psi)
+
+function cost_func(psidata::Array{Float64, 3})
+    psi = data_to_TensorMap(psidata)
+    Tpsi = act(T, psi)
+    up = ovlp(Tpsi, Tpsi) * ovlp(psi, psi)
+    dn = ovlp(psi, Tpsi) * ovlp(Tpsi, psi)
+    return log(norm(up / dn))
+end
+
+function cost_func_grad!(grad::Array{Float64, 3}, psidata::Array{Float64, 3})
+    grad .= gradient(cost_func, psidata)[1]
+end
+
+psidata = rand(Float64, (2, 2, 2))
+io = open("result.txt", "w+")
+for chi in [2, 4, 8, 16, 32, 64, 128] 
+    res_f = optimize(cost_func, cost_func_grad!, psidata, LBFGS(), Optim.Options(show_trace=true, iterations=200))
+    psidata_final = Optim.minimizer(res_f)
+    psi_final = data_to_TensorMap(psidata_final)
+    F_final = ovlp(psi_final, act(T, psi_final)) / ovlp(psi_final, psi_final)
+    println(io, size(psidata), ' ', log(F_final |> real))
+    psi = act(T, psi_final)
+    psidata = reshape(psi.data, (2*chi, 2, 2*chi))
+end
+close(io)
+
+function abs_diag(m::TensorMap{CartesianSpace, 1, 1})
+    m_diag = Diagonal(m.data)
+    chi = dim(domain(m))
+
+    factor = sign(m_diag[1, 1])
+    for ix in 1:chi
+        m_diag[ix, ix] = abs(m_diag[ix, ix])
+    end
+    return TensorMap(Matrix(m_diag), ℝ^chi, ℝ^chi)
+end
+
+function pseudo_inv(m::TensorMap{CartesianSpace, 1, 1})
+    m_diag = Diagonal(m.data)
+    chi = dim(domain(m))
+    for ix in 1:chi
+        if abs(m_diag[ix, ix]) > 1e-8
+            m_diag[ix, ix] = 1 / abs(m_diag[ix, ix])
+        else
+            m_diag[ix, ix] = 0    
+        end
+    end
+    return TensorMap(Matrix(m_diag), ℝ^chi, ℝ^chi)
+end
+
+V = [1; 2; 3e-10]
+a_diag = diagm(V)
+a_diag = TensorMap(a_diag, ℝ^3, ℝ^3)
+pseudo_inv(a_diag)
+
+##### power method, use iTEBD
+function idmrg(psi::TensorMap{CartesianSpace, 2, 1}, target_chi::Integer)
+    lop = transf_mat(psi, psi)
+    lopT = transf_mat_T(psi, psi)
+    chi = dim(domain(psi))
+    v0 = Tensor(rand, Float64, ℝ^chi*ℝ^chi)
+
+    wr, vr = eigsolve(lop, v0, 1)
+    wr = wr[1]
+    vr = vr[1] / vr[1][1,1]
+
+    _, vl = eigsolve(lopT, v0, 1)
+    vl = vl[1] / vl[1][1,1]
+
+    vr = permute(vr, (1,), (2,))
+    vl = permute(vl, (1,), (2,))
+    @assert vr ≈ vr'
+    @assert vl ≈ vl'
+
+    λr, Ur = eigh(vr)
+    for ix in 1:chi
+        @assert λr[ix, ix] * λr[1,1] > 0
+    end
+    λr = abs_diag(λr)
+    Xr = Ur * sqrt(λr)
+    Xr_inv = sqrt(pseudo_inv(λr)) * Ur'
+
+    λl, Ul = eigh(vl)
+    λl = abs_diag(λl)
+    Xl = Ul * sqrt(λl)
+    Xl_inv = sqrt(pseudo_inv(λl)) * Ul'
+
+    λ1 = permute(Xl, (2,), (1,)) * Xr
+    Γ1 = @ncon((Xr_inv, psi, Xl_inv'), 
+        ([-1, 1], [1, -2, 2], [2, -3])
+    )
+    Γ1 = permute(Γ1, (1, 2), (3,))
+
+    u, λ2, v = tsvd(λ1)
+    λ2 /= sqrt(real(abs(wr))) # renormalize
+    Γ2 = @ncon((v, Γ1, u), 
+        ([-1, 1], [1, -2, 2], [2, -3])
+    )
+    Γ2 = permute(Γ2, (1, 2), (3,))
+
+    psi_canonical = Γ2 * λ2
+
+    # truncation
+    truncation_tensor = isometry(ℝ^chi, ℝ^target_chi)
+    @tensor psi_truncated[-1, -2; -3] :=  truncation_tensor[1, -1] * psi_canonical[1, -2, 2] * truncation_tensor[2, -3]
+    #psi_truncated = permute(psi_truncated, (1, 2), (3,))
+
+    # estimated scale of error
+    if target_chi == chi
+        error_scale = 0
+    else
+        error_scale = λ2[target_chi+1, target_chi+1]
+    end
+    return psi_truncated, sqrt(real(wr)), error_scale
+end
+#@VSCodeServer.run idmrg(psi, 7)
+psi = TensorMap(rand, Float64, ℝ^8*ℝ^2, ℝ^8)
+psi_1, _, _ = idmrg(psi, 7)
+psi_2, _, _ = idmrg(psi_1, 7)
+lop = transf_mat(psi_1, psi_1)
+lop2 = transf_mat(psi_2, psi_2)
+w2, v2 = eigsolve(lop2, Tensor(rand, Float64, ℝ^7*ℝ^7), 1)
+w2, v2 = w2[1], v2[1]
+v2 = permute(v2, (1, ), (2, ))
+v2 = v2 / v2[1,1]
+
+w1, v1 = eigsolve(lop, Tensor(rand, Float64, ℝ^8*ℝ^8), 1)
+w1, v1 = w1[1], v1[1]
+v1 = permute(v1, (1, ), (2,))
+v1
+v1[1,1]
+v1[2,2]
+v1[3,3]
+v1[4,4]
+v1
+svd(v1)
+
+
+v1 = v1 / v1[1,1]
+v1 - v1'
+
+
+
+
+psi
+Tpsi=act(T, psi)
+psi, _, _ = idmrg(Tpsi, 8)
+
+ovlp(Tpsi, psi) * ovlp(psi, Tpsi) / (ovlp(Tpsi, Tpsi) * ovlp(psi, psi))
+
+psi = TensorMap(rand, Float64, ℝ^8*ℝ^2, ℝ^8)
+for ix in 1:10
+    Tpsi = act(T, psi)
+    psi, norm_psi, error = idmrg(Tpsi, 8)
+    F_value = ovlp(psi, act(T, psi)) / ovlp(psi, psi)
+    println(ix, ' ', log(F_value |> norm), ' ', norm_psi, ' ', error)
+end
+Tpsi = act(T, psi)
+idmrg(Tpsi, 8)
+
+
+# tests for idmrg
+ovlp(psi_new, psi) * ovlp(psi, psi_new) / ovlp(psi, psi)
+
+@tensor right_contr[:] := Γ2'[3, -2, 4] * λ2'[2, 3] * λ2[1, 2] * Γ2[-1, 4, 1]
+right_contr = permute(right_contr, (1,), (2,))
+right_contr ≈ id(ℝ^8)
+
+function mps_plus(psi::TensorMap{CartesianSpace, 2, 1}, phi::TensorMap{CartesianSpace, 2, 1})
+    chi_psi, chi_phi = dim(domain(psi)), dim(domain(phi))
+    chi = chi_psi + chi_phi
+
+    embedder1 = isometry(ℝ^chi, ℝ^chi_psi)
+    embedder2 = TensorMap(zeros, Float64, ℝ^chi, ℝ^chi_phi)
+    embedder2.data[chi_psi+1:chi, :] = Matrix{Float64}(I, chi_phi, chi_phi)
+
+    @tensor psi_plus_phi[-1, -2; -3] := embedder1[-1, 1] * psi[1, -2, 2] * embedder1[-3, 2] +
+             embedder2[-1, 1] * phi[1, -2, 2] * embedder2[-3, 2]
+
+    return psi_plus_phi
+end
+
+psi = TensorMap(rand, Float64, ℝ^4*ℝ^2, ℝ^4)
+phi = TensorMap(rand, Float64, ℝ^4*ℝ^2, ℝ^4)
+psi_add_phi = mps_plus(psi, phi)
+psi_add_phi_arr = reshape(psi_add_phi.data, (8, 2, 8))
+psi_add_phi_arr[:, 1, :]
+
+psi = TensorMap(rand, Float64, ℝ^2*ℝ^2, ℝ^2)
+for ix in 1:20
+    Tpsi = act(T, psi)
+    Tpsi, _, _ = idmrg(Tpsi, 2) # normalize Tpsi
+    psi, norm_psi, error = idmrg(mps_plus(Tpsi, psi), 2)
+    F_value = ovlp(psi, act(T, psi)) / ovlp(psi, psi)
+    println(" --- ")
+    println(ix, ' ', log(F_value |> norm), ' ', norm_psi, ' ', error)
+    println(" --- ")
+end
+
+psi = TensorMap(rand, Float64, ℝ^8*ℝ^2, ℝ^8)
+Tpsi = act(T, psi)
+
+Tpsi1, norm_Tpsi, em_diag,rror = idmrg(Tpsi, 16)
