a4a44fc83a70bda8f46f8b124add734323145b60
Wei Tang <wei.tang.nju@gmail.com>
2021-11-10 14:59:22 +0000

    start to work on bi-directional alg

--- src/bi_direction.jl
+++ src/bi_direction.jl
@@ -1,5 +1,14 @@
-struct bi_projector
-    A::TensorMap{ComplexF64, 2, 1}
-    B::TensorMap{ComplexF64, 2, 1}
+# algorithm 4 in section 5.4 of https://www.annualreviews.org/doi/10.1146/annurev-conmatphys-031016-025507
+
+struct bi_mps
+    A::TensorMap{ComplexSpace, 2, 1}
+    B::TensorMap{ComplexSpace, 2, 1}
 end
+
+function A_canonical(bi_psi::bi_mps)
+    
+
+end
+
+
 
