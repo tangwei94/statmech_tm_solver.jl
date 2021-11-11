764cbaea95d12e21744ac5a4f70848fe29fca8ab
Wei Tang <wei.tang.nju@gmail.com>
2021-11-11 23:08:04 +0000

    fix error in bi_direction

--- src/bi_direction.jl
+++ src/bi_direction.jl
@@ -13,39 +13,39 @@ end
 
 function A_canonical(T::TensorMap{ComplexSpace, 2, 2}, psi::bimps, tol::Float64=1e-15)
 
-    # convert A to left canonical form
-    X, AL = left_canonical_QR(psi.A, tol)
-    Xinv = inv(X)
-    @tensor BL[-1, -2; -3] := psi.B[1, -2, 2] * X[-1, 1] * Xinv[2, -3] 
+    # convert A to right canonical form
+    Y, AR = right_canonical_QR(psi.A, tol)
+    Yinv = inv(Y')'
+    @tensor BR[-1, -2; -3] := psi.B[1, -2, 2] * Yinv'[-1, 1] * Y'[2, -3]  
 
     # construct the linear operator for the maping of B
     function lop(v::TensorMap{ComplexSpace, 2, 1}) 
-        @tensor Tv[-1, -2; -3] := v[1, 3, 4] * AL'[-1, 1, 2] * AL[4, 5, -3] * T[-2, 2, 5, 3]
+        @tensor Tv[-1, -2; -3] := v[1, 3, 4] * AR'[4, -3, 5] * AR[-1, 2, 1] * T[-2, 5, 2, 3]
         return Tv 
     end 
 
-    # solve BL from the fixed point equation
-    _, BL = eigsolve(lop, BL, 1)
-    BL = BL[1]
-    return bimps(AL, BL)
+    # solve BR from the fixed point equation
+    _, BR = eigsolve(lop, BR, 1)
+    BR = BR[1]
+    return bimps(AR, BR)
 end
 
 function B_canonical(T::TensorMap{ComplexSpace, 2, 2}, psi::bimps, tol::Float64=1e-15)
 
-    # convert B to right canonical form
-    Y, BR = right_canonical_QR(psi.B, tol)
-    Yinv = inv(Y')'
-    @tensor AR[-1, -2; -3] := Yinv'[-1, 1] * psi.A[1, -2, 2] * Y'[2, -3]
+    # convert B to left canonical form
+    X, BL = left_canonical_QR(psi.B, tol)
+    Xinv = inv(X)
+    @tensor AL[-1, -2; -3] := X[-1, 1] * psi.A[1, -2, 2] * Xinv[2, -3]
 
     # construct the linear operator for the maping of A
     function lop(v::TensorMap{ComplexSpace, 2, 1}) 
-        @tensor Tv[-1, -2; -3] := AR[1, 2, 4] * BR[-1, 3, 1] * T[5, -2, 2, 3] * BR'[4, -3, 5]
+        @tensor Tv[-1, -2; -3] := v[4, 2, 1] * BL[1, 3, -3] * T[5, -2, 2, 3] * BL'[-1, 4, 5]
         return Tv 
     end 
 
     # solve AR from the fixed point equation
-    _, AR = eigsolve(lop, AR, 1)
-    AR = AR[1]
-    return bimps(AR, BR)
+    _, AL = eigsolve(lop, AL, 1)
+    AL = AL[1]
+    return bimps(AL, BL)
 end
 
