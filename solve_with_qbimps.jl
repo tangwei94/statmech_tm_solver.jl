0c610a2fc23788a54e9afb6ec16b6279aac64db3
Wei Tang <wei.tang.nju@gmail.com>
2021-11-17 23:48:29 +0000

    loop over chi in solve_with_qbimps; check energy

--- solve_with_qbimps.jl
+++ solve_with_qbimps.jl
@@ -1,32 +1,37 @@
+using Revise
 using TensorKit
 using statmech_tm_solver
+using Printf
 
-chi = 64
-psi = qbimps(rand, chi, 2, 3)
-Δ = 1.0 
-
+Δ = -0.5 
 T = cmpo_xxz(Δ)
 println("========================================================================")
 
-ix, δ = 0, 42.
-while ix < 50 && δ > 1e-12
-    ΛA, psi = A_canonical(T, psi)
-    ΛB, psi = B_canonical(T, psi)
-    ix += 1
-    δ = norm(ΛA - ΛB) / norm(ΛA + ΛB) 
-    println("$ix, ΛA: $ΛA, ΛB: $ΛB, δ: $δ")
-end
+psi = qbimps(rand, 2, 2, 3)
+for chi in [2; 4; 8; 16; 32; 64; 128]
+    psi = expand(psi, chi)
+    println("===========================  chi = $chi   ==============================")
+    ix, δ = 0, 42.
+    while ix < 100 && δ > 1e-12
+        ΛA, psi = A_canonical(T, psi)
+        ΛB, psi = B_canonical(T, psi)
+        ix += 1
+        δ = norm(ΛA - ΛB) / norm(ΛA + ΛB) 
+        println("$ix, ΛA: $ΛA, ΛB: $ΛB, δ: $δ")
+        println("energy density: ", energy_quantum_xxz(psi.A, Δ))
+    end
 
-sA = entanglement_spectrum(psi.A)
-sB = entanglement_spectrum(psi.B)
+    sA = entanglement_spectrum(psi.A)
+    sB = entanglement_spectrum(psi.B)
 
-io = open("result_qbimps_chi$(chi)_xxz_Delta$(Δ).txt", "w")
-for s in sA
-    print(io, "$s ")
+    io = open(@sprintf("result_qbimps_chi%d_xxz_Delta%.2f.txt", chi, Δ), "w")
+    for s in sA
+        print(io, "$s ")
+    end
+    println(io, " ")
+    for s in sB
+        print(io, "$s ")
+    end
+    println(io, " ")
+    close(io)
 end
-println(io, " ")
-for s in sB
-    print(io, "$s ")
-end
-println(io, " ")
-close(io)
