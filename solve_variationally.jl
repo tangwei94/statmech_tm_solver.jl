290252f1105eb247a4a91d70d0bc69c73311afb5
Wei Tang <wei.tang.nju@gmail.com>
2021-12-16 12:52:26 +0100

    obtain left eigenvector variationally

--- solve_variationally.jl
+++ solve_variationally.jl
@@ -10,8 +10,9 @@ using statmech_tm_solver
 
 # MPO for the triangular AF Ising
 T = mpo_triangular_AF_ising()
-T2 = mpo_triangular_AF_ising_alternative()
-T_adapter1, T_adapter = mpo_triangular_AF_ising_adapter()
+T_adj = reshape(T.data, (2, 2, 2, 2))
+T_adj = permutedims(conj.(T_adj), (1, 3, 2, 4))
+T_adj = TensorMap(T_adj, ℂ^2*ℂ^2, ℂ^2*ℂ^2)
 
 function f(arr::Array{ComplexF64, 3})
     phi = arr_to_TensorMap(arr)
@@ -19,31 +20,44 @@ function f(arr::Array{ComplexF64, 3})
     return nonherm_cost_func(T, phi)
 end
 
+function fT(arr::Array{ComplexF64, 3})
+    phi = arr_to_TensorMap(arr)
+    return nonherm_cost_func(T_adj, phi)
+end
+
 function g!(grad::Array{ComplexF64, 3}, psidata::Array{ComplexF64, 3})
     grad .= gradient(f, psidata)[1]
 end
 
+function gT!(grad::Array{ComplexF64, 3}, psidata::Array{ComplexF64, 3})
+    grad .= gradient(fT, psidata)[1]
+end
+
 psidata = rand(MersenneTwister(1), ComplexF64, (2, 2, 2))
 
-io = open("result_variational.txt", "w")
-close(io)
-for chi in [2, 4, 8, 16]
+#io = open("result_variational.txt", "w")
+#close(io)
+for chi in [2, 4, 8, 16, 32, 64, 128, 256]
     global psidata 
     io = open("result_variational.txt", "a+")
 
     println(size(psidata))
 
     res_f = optimize(f, g!, psidata, LBFGS(), Optim.Options(show_trace=true, iterations=200))
+    res_fT = optimize(fT, gT!, psidata, LBFGS(), Optim.Options(show_trace=true, iterations=200))
 
     psidata_final = Optim.minimizer(res_f)
     psi = arr_to_TensorMap(psidata_final)
 
+    psiLdata_final = Optim.minimizer(res_fT)
+    psiL = arr_to_TensorMap(psiLdata_final)
+
     cost_func_final = nonherm_cost_func(T, psi)
-    F_final = free_energy(T, psi)
-    F_final_2 = free_energy(T2, act(T_adapter, psi))
-    cost_func_final2 = nonherm_cost_func(T2, act(T_adapter, psi))
-    println(io, chi, ' ', F_final, ' ', cost_func_final, ' ', F_final_2, ' ', cost_func_final2 )
+    F_final_1 = free_energy(T, psi)
+    F_final = ovlp(psiL, act(T, psi)) / ovlp(psiL, psi) |> norm |> log
+    println(io, chi, ' ', F_final, ' ', F_final_1, ' ', cost_func_final )
     quicksave("ckpt_variational_chi$(chi)", psi)    
+    quicksave("ckpt_variational_left_chi$(chi)", psiL)    
 
     psidata = toarray(act(T, psi))
 
